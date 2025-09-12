76995215a07760209b5b8a1ebcc7dbaf608726da
sidhartha-mandal <msidhartha10@gmail.com>
2025-09-12 19:14:03 +0530

    re type the js code start with generateArray

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-const arrayContainer = document.getElementById("array-container");
+let array = [];
+const container = document.getElementById("array-container");
 
-const generateArray = () => {
-    arrayContainer.innerHTML = "";
-    for (let i = 0; i < 30; i++) {
-        const barHeight = ~~(Math.random() * 80) + 20; // this is to calculate the random height between 20 to 100 percent.
-
-        const arrayBar = document.createElement("div");
-        arrayBar.classList.add("array-bar");
-        arrayBar.style.height = `${barHeight}%`;
-        arrayContainer.appendChild(arrayBar);
-
-    }
-}
-const Bubble_sort = async () => {
-    const bars = document.querySelectorAll(".array-bar");
-    for (let i = 0; i < bars.length; i++) {
-        for (let j = 0; j < bars.length - i - 1; j++) {
-            bars[j].style.backgroundColor = "#ff6f61";
-            bars[j + 1].style.backgroundColor = "#ff6f61";
-            if (parseInt(bars[j], style.height) > parseInt(bars[j + 1].style.height)) {
-                await swap(bars[j], bars[j + 1]);
-            }
-            bars[j].style.backgroundColor = "white"; //reset colors after comparision
-            bars[j + 1].style.backgroundColor = "white";
-        }
-        bars[bars.length - i - 1].style.backgroundColor = "#6b6b6b"
-    }
+function generateArray(size = 40) {
+  array = [];
+  container.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    array[i] = Math.floor(Math.random() * 200) + 20;
+    const bar = document.createElement("div");
+    bar.style.height = `${array[i]}px`;
+    bar.classList.add("bar");
+    container.appendChild(bar);
+  }
 }
 
-const swap = (bar1, bar2) => {
-    return new Promise(resolve => {
-    setTimeout(() => {
-      let temp = el1.style.height;
-      el1.style.height = el2.style.height;
-      el2.style.height = temp;
-      resolve();
-    }, 45); //   delay for the visualization
-  });
-}
-generateArray();
 
