7e6575ec12c78b5dc7a0c428d7c2799b2896f0f1
sidhartha-mandal <msidhartha10@gmail.com>
2025-09-12 19:18:57 +0530

    adjusting the html code and adding the swap function

--- script.js
+++ script.js
@@ -12,5 +12,31 @@ function generateArray(size = 40) {
     container.appendChild(bar);
   }
 }
-
+async function swap(el1, el2) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      let temp = el1.style.height;
+      el1.style.height = el2.style.height;
+      el2.style.height = temp;
+      resolve();
+    }, 45);
+  });
+}
+// Bubble Sort
+async function bubble_Sort() {
+  const bars = document.querySelectorAll(".bar");
+  for (let i = 0; i < bars.length; i++) {
+    for (let j = 0; j < bars.length - i - 1; j++) {
+      bars[j].style.background = "red";
+      bars[j + 1].style.background = "red";
+      if (parseInt(bars[j].style.height) > parseInt(bars[j + 1].style.height)) {
+        await swap(bars[j], bars[j + 1]);
+      }
+      bars[j].style.background = "#17a2b8";
+      bars[j + 1].style.background = "#17a2b8";
+    }
+    bars[bars.length - i - 1].style.background = "green";
+  }
+}
+generateArray();
 
