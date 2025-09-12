a85686a9397e7f16146752c51df803cc821f4e0d
sidhartha-mandal <msidhartha10@gmail.com>
2025-09-12 19:24:24 +0530

    minor changes in body

--- script.js
+++ script.js
@@ -22,21 +22,5 @@ async function swap(el1, el2) {
     }, 45);
   });
 }
-// Bubble Sort
-async function bubble_Sort() {
-  const bars = document.querySelectorAll(".bar");
-  for (let i = 0; i < bars.length; i++) {
-    for (let j = 0; j < bars.length - i - 1; j++) {
-      bars[j].style.background = "red";
-      bars[j + 1].style.background = "red";
-      if (parseInt(bars[j].style.height) > parseInt(bars[j + 1].style.height)) {
-        await swap(bars[j], bars[j + 1]);
-      }
-      bars[j].style.background = "#17a2b8";
-      bars[j + 1].style.background = "#17a2b8";
-    }
-    bars[bars.length - i - 1].style.background = "green";
-  }
-}
-generateArray();
+
 
